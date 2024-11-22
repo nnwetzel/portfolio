825a5f490c13fdee7eb59cbfb240f6ed411b0542
Nat Wetzel <wetzel.na@northeastern.edu>
2024-11-22 04:04:59 -0800

    Add word

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
           fleet of drones to prevent wildfires in Australia.
         </p>
         <p className="mb-4">
-          I’m passionate about <b>problem-solving</b> and <b>creative engineering</b>, viewing
+          Therefore, I’m passionate about <b>problem-solving</b> and <b>creative engineering</b>, viewing
           every opportunity as a stepping stone toward meaningful change. During my Amazon
           internship as an SDE and as a recipient of the Amazon Future Engineer Scholarship, I
           strengthened my passion for <b>software engineering</b> and developed a strong foundation in
