1837a0b53a90d5a047d94e6f53046206a7dfeee2
Nat Wetzel <wetzel.na@northeastern.edu>
2024-11-22 11:37:02 -0800

    About me update

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <p className="mb-4">
           As a second-year Computer Science student at Northeastern University, concentrating in
           Artificial Intelligence, I’ve seen firsthand how computer science is transforming
-          the world. From platforms like Khan Academy making education accessible to
+          the <i>world</i>. From platforms like Khan Academy making education accessible to
           artificial intelligence forecasting the spread of COVID-19, the possibilities are endless.
         </p>
         <p className="mb-4">
@@ -17,15 +17,17 @@ export default function Page() {
           fleet of drones to prevent wildfires in Australia.
         </p>
         <p className="mb-4">
-          Therefore, I’m passionate about <b>problem-solving</b> and <b>creative engineering</b>, viewing
+          Therefore, I’m passionate about <b>problem-solving</b> and <b>creative engineering</b>,
+          viewing
           every opportunity as a stepping stone toward meaningful change.
         </p>
         <p className="mb-4">
-          During my Amazon
-          internship as an SDE and as a recipient of the Amazon Future Engineer Scholarship, I
-          strengthened my passion for <b>software engineering</b> and developed a strong foundation in
-          <b>designing innovative solutions</b>. I’m excited to further explore opportunities that
-          challenge me to grow my skills and contribute to solving pressing global challenges.
+          During my Amazon internship as an SDE and as a recipient of the Amazon Future Engineer
+          Scholarship, I
+          strengthened my passion for <b>software engineering</b> and developed a strong foundation
+          in <b>designing innovative solutions</b>.
+          I’m excited to further explore opportunities that challenge me to grow my skills and
+          contribute to solving pressing global challenges.
         </p>
         <p className="mb-4">
           In the future, I see myself working as a full-time software engineer. Regardless of the
