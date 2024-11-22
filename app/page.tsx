d26ec22bc19e759d0380ef61bd98c1c963dc4765
Nat Wetzel <wetzel.na@northeastern.edu>
2024-11-22 03:55:14 -0800

    Add bold and italicized text

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,27 @@ export default function Page() {
           Hi, I'm Nathaniel
         </h1>
         <p className="mb-4">
-          As a second-year Computer Science student at Northeastern University, concentrating in Artificial Intelligence, I’ve seen firsthand how computer science is transforming the world. From platforms like Khan Academy making education accessible to artificial intelligence forecasting the spread of COVID-19, the possibilities are endless.
+          As a second-year Computer Science student at Northeastern University, concentrating in
+          Artificial Intelligence, I’ve seen firsthand how computer science is transforming
+          the world. From platforms like Khan Academy making education accessible to
+          artificial intelligence forecasting the spread of COVID-19, the possibilities are endless.
         </p>
         <p className="mb-4">
-          I’ve even seen how AWS-powered machine learning models operate a fleet of drones to prevent wildfires in Australia.
+          I’ve even seen how machine learning models powered by Amazon Web Services (AWS) operate a
+          fleet of drones to prevent wildfires in Australia.
         </p>
         <p className="mb-4">
-          I’m passionate about problem-solving and creative engineering and view every opportunity as a stepping stone toward creating meaningful change. During my Amazon internship as an SDE and as a recipient of the Amazon Future Engineer Scholarship, I deepened my love for software engineering and developed a strong foundation in designing innovative solutions. I’m excited to further explore opportunities that challenge me to grow my skills and contribute to solving pressing global challenges.
+          I’m <b>passionate</b> about <b>problem-solving</b> and <b>creative engineering</b>, viewing
+          every opportunity as a stepping stone toward meaningful change. During my Amazon
+          internship as an SDE and as a recipient of the Amazon Future Engineer Scholarship, I
+          strengthened my <b>passion</b> for <b>software engineering</b> and developed <b>a strong foundation</b> in
+          <b>designing innovative solutions</b>. I’m excited to further explore opportunities that
+          challenge me to grow my skills and contribute to solving pressing global challenges.
         </p>
         <p className="mb-4">
-          In the future, I see myself as a full-time software engineer. However, regardless of where I end up, my experience and dedication to computer science will empower me to make a substantial impact on the world.
+          In the future, I see myself working as a full-time software engineer. Regardless of the
+          path I take, my <b>dedication</b> to computer science will allow me to make a meaningful
+          impact on the <i>world</i>.
         </p>
       </section>
   )
