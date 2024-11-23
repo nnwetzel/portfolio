17c343ce0e896d444c046a5d5c951947e9cf52e9
Nat Wetzel <wetzel.na@northeastern.edu>
2024-11-22 19:12:06 -0800

    Experience update

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,9 +1,20 @@
 export default function ExperiencePage() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Experience
-      </h1>
-    </section>
+      <section>
+        <h1 className="mb-8 text-3xl font-semibold tracking-tighter">
+          Experience
+        </h1>
+        <section className="mb-8">
+          <div className="flex justify-between items-start">
+            <h2 className="text-xl font-semibold tracking-tighter">Amazon</h2>
+            <span className="text-sm text-neutral-500">2024</span>
+          </div>
+          <p className="mb-4">
+            Designed and implemented scalable features using AWS services, including Lambda and
+            EventBridge. Strengthened technical skills in system design, cloud computing, and software
+            engineering.
+          </p>
+        </section>
+      </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
       <section>
-        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        <h1 className="mb-8 text-3xl font-semibold tracking-tighter">
           Hi, I'm Nathaniel
         </h1>
         <p className="mb-4">
