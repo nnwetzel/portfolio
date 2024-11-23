d08deefcc00ec03d600bcf5db8a9b61b7fbe69a4
Nat Wetzel <wetzel.na@northeastern.edu>
2024-11-22 18:46:13 -0800

    Add experience

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const navItems = {
   '/': {
@@ -13,31 +14,36 @@ const navItems = {
   '/projects': {
     name: 'projects',
   },
-}
+};
 
 export function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              )
-            })}
-          </div>
-        </nav>
-      </div>
-    </aside>
-  )
+      <aside className="-ml-[8px] mb-16 tracking-tight">
+        <div className="lg:sticky lg:top-20">
+          <nav
+              className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+              id="nav"
+          >
+            <div className="flex flex-row space-x-0 pr-10">
+              {Object.entries(navItems).map(([path, { name }]) => {
+                const isActive = pathname === path;
+                return (
+                    <Link
+                        key={path}
+                        href={path}
+                        className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1 ${
+                            isActive ? 'text-neutral-800 dark:text-neutral-200 font-bold' : ''
+                        }`}
+                    >
+                      {name}
+                    </Link>
+                );
+              })}
+            </div>
+          </nav>
+        </div>
+      </aside>
+  );
 }
