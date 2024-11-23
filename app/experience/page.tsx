362d49c83008e4eb11f577a1773baadc50fe586e
Nat Wetzel <wetzel.na@northeastern.edu>
2024-11-22 19:18:59 -0800

    Update experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,9 +5,13 @@ export default function ExperiencePage() {
           Experience
         </h1>
         <section className="mb-8">
-          <div className="flex justify-between items-start">
+          <div className="flex justify-between items-center mb-2">
             <h2 className="text-xl font-semibold tracking-tighter">Amazon</h2>
-            <span className="text-sm text-neutral-500">2024</span>
+            <span className="text-xl text-neutral-500">May - Aug 2024</span>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-lg text-neutral-600">Software Development Engineer Intern</p>
+            <p className="text-sm text-neutral-500">Seattle, WA</p>
           </div>
           <p className="mb-4">
             Designed and implemented scalable features using AWS services, including Lambda and
